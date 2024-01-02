cd46eac7a45fa51dda390d09c664b325cb59fdad
zkFriendly <zkfriendly@skiff.com>
2024-01-02 18:00:13 +0100

    added lookup table column to table

--- halo2/src/range_check/eg3_numbits/table.rs
+++ halo2/src/range_check/eg3_numbits/table.rs
@@ -10,17 +10,34 @@ use halo2_proofs::{
 #[derive(Clone)]
 pub(super) struct RangeCheckTable<F: FieldExt, const NUM_BITS: usize> {
     pub(super) value: TableColumn,
+    pub(super) num_bits: TableColumn,
     _marker: std::marker::PhantomData<F>,
 }
 
 impl<F: FieldExt, const NUM_BITS: usize> RangeCheckTable<F, NUM_BITS> {
     pub(super) fn configure(meta: &mut ConstraintSystem<F>) -> Self {
         let value = meta.lookup_table_column();
+        let num_bits = meta.lookup_table_column();
 
         Self {
             value,
+            num_bits,
             _marker: std::marker::PhantomData,
         }
+    }
+
+    fn log2(&self, x: u64) -> u64 {
+        // implement log 2 of x
+        // https://stackoverflow.com/questions/11376288/fast-computing-of-log2-for-64-bit-integers
+        let mut x = x;
+        let mut result = 0;
+
+        while x > 1 {
+            x >>= 1;
+            result += 1;
+        }
+
+        result
     }
 
     pub(super) fn load(&self, mut layouter: impl Layouter<F>) -> Result<(), Error> {
@@ -30,6 +47,15 @@ impl<F: FieldExt, const NUM_BITS: usize> RangeCheckTable<F, NUM_BITS> {
                 let mut offset = 0;
                 for i in 0..(1 << NUM_BITS) {
                     table.assign_cell(|| "assign cell", self.value, offset, || Ok(F::from(i)))?;
+
+                    let num_bits = self.log2(i);
+
+                    table.assign_cell(
+                        || "numbits table",
+                        self.num_bits,
+                        offset,
+                        || Ok(F::from(num_bits)),
+                    )?;
                     offset = offset + 1;
                 }
 
