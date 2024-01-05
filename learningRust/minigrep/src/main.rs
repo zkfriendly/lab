7583cfcdfc42115a4c8a3b178a082fbf1e9ff0f8
zkFriendly <zkfriendly@skiff.com>
2024-01-05 23:23:19 +0100

    command line parser

--- learningRust/minigrep/src/main.rs
+++ learningRust/minigrep/src/main.rs
@@ -1,12 +1,19 @@
 use std::env;
 use std::fs;
+use std::process;
 
 mod minigrep;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let grep = minigrep::build(args).unwrap();
+    let grep = minigrep::build(args).unwrap_or_else(|e| {
+        eprintln!("{e}");
+        process::exit(1);
+    });
 
-    minigrep::run(grep).unwrap();
+    minigrep::run(grep).unwrap_or_else(|e| {
+        eprintln!("{e}");
+        process::exit(1);
+    });
 }
